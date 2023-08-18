7e12a3b6cc5e157e68c90a958b9a3aa057a79323
minsang <minsang0610@naver.com>
2023-08-18 17:38:23 +0900

    [ADD] Equip ON/OFF

--- ConsoleDungeon/Program.cs
+++ ConsoleDungeon/Program.cs
@@ -1,67 +1,124 @@
 ﻿using System;
 using System.Collections.Generic;
 
-
-public class Player
+// 캐릭터 인터페이스 : 플레이어, 몬스터들의 기본 스탯
+public interface ICharacter
 {
-    private string name;
-    private string job;
-    private int level;
-    private int ap;
-    private int dp;
-    private int hp;
-    private int speed;
-    private int gold;
-    private List<Equip> inventory;
+    string Name { get; }
+    string Job { get; }
+    int Level { get; }
+    int AP { get; }
+    int DP { get; }
+    int HP { get; }
+    int Speed { get; }
+}
 
-    public string Name => name;
-    public string Job => job;
-    public int Level => level;
-    public int AP => ap;
-    public int DP => dp;
-    public int HP => hp;
-    public int Speed => speed;
-    public int Gold => gold;
-    public List<Equip> Inventory => inventory;
+public class Player : ICharacter
+{
+    public string Name { get; private set; }
+    public string Job { get; private set; }
+    public int Level { get; private set; }
+    public int AP { get; private set; }
+    public int DP { get; private set; }
+    public int HP { get; private set; }
+    public int Speed { get; private set; }
+    public int Gold { get; private set; }
+    public List<Equip> Inventory { get; private set; }
+
+    public List<Equip> OnEquip { get; set; }
 
     public Player(string _name, string _job, int _ap, int _dp, int _hp, int _speed)
     {
-        name = _name;
-        job = _job;
-        level = 1;
-        ap = _ap;
-        dp = _dp;
-        hp = _hp;
-        speed = _speed;
-        gold = 0;
-        inventory = new List<Equip>() { new Equip("무쇠 갑옷", "방어력 +5", "무쇠로 만든 갑옷"), new Equip("낡은 검", "공격력 +2", "기본 중에 기본") };
+        Name = _name;
+        Job = _job;
+        Level = 1;
+        AP = _ap;
+        DP = _dp;
+        HP = _hp;
+        Speed = _speed;
+        Gold = 0;
+        Inventory = new List<Equip>()
+        {
+            new Equip("무쇠 갑옷", EquipType.Armor, "방어력 +5", "무쇠로 만들어져 튼튼한 갑옷입니다."),
+            new Equip("낡은 검", EquipType.Weapon, "공격력 +2", "쉽게 볼 수 있는 낡은 검입니다."),
+            new Equip("빛나는 검", EquipType.Weapon, "공격력 +7", "반짝반짝.")
+        };
+        OnEquip = new List<Equip>() { null, null, null, null };
     }
+
+    public string ShowEquip(int idx)
+    {
+        if (OnEquip[idx] == null)
+            return " ";
+        else
+            return OnEquip[idx].Name;
+    }
+}
+
+public enum EquipType
+{
+    Armor = 0,
+    Weapon = 1,
+    Accessory = 2,
+    Item = 3,
 }
 
 public class Equip
 {
-    private string name;
-    private string effect;
-    private string sub;
-    public char state;
+    public string Name { get; private set; }
+    public EquipType Type { get; private set; }
+    public string Effect { get; private set; }
+    public string Sub { get; private set; }
+    public char State { get; private set; }
 
-    public string Name => name;
-    public string Effect => effect;
-    public string Sub => sub;
-    public char State => state;
+    private bool isEquipped = false;
 
-    public Equip(string _name, string _effect, string _sub)
+    public Equip(string _name, EquipType _type, string _effect, string _sub)
     {
-        name = _name;
-        effect = _effect;
-        sub = _sub;
-        state = ' ';
+        Name = _name;
+        Type = _type;
+        Effect = _effect;
+        Sub = _sub;
+        State = ' ';
+    }
+
+    public void Equipped(Player player)
+    {
+        // 장착
+        if (!isEquipped)
+        {
+            // 이미 해당 타입의 장비가 장착되어 있을 때 해제 먼저
+            if (player.OnEquip[(int)Type] != null)
+            {
+                player.OnEquip[(int)Type].State = ' ';
+                player.OnEquip[(int)Type].isEquipped = false;
+                player.OnEquip[(int)Type] = null;
+            }                
+
+            player.OnEquip[(int)Type] = this;
+            State = 'E';
+        }
+        // 해제
+        else
+        {
+            player.OnEquip[(int)Type] = null;
+            State = ' ';
+        }
+
+        isEquipped = !isEquipped;
     }
 }
 
 
 public class GameController
 {
+    Player player;
+
+    public GameController(Player _player)
+    {
+        player = _player;
+    }
+
     // 입력 받는 함수 : min <= input <= max 값만 받도록
     private int GetInput(int min, int max)
     {
@@ -83,44 +140,83 @@ public class GameController
 
     public void VillageEnterance()
     {
-        Console.WriteLine("====================================");
-        Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
-        Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.\n");
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("====================================");
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+            Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.\n");
 
-        Console.WriteLine("1. 상태보기");
-        Console.WriteLine("2. 인벤토리");
+            Console.WriteLine("1. 상태보기");
+            Console.WriteLine("2. 인벤토리");
+            Console.WriteLine("\n0. 게임 종료");
 
-        int input = GetInput(1, 2);
 
-        Console.WriteLine(input);
+            int input = GetInput(0, 2);
+
+            if (input == 1) ShowStatus();
+            else if (input == 2) ShowInventory();
+            else return;
+        }
     }
 
-    public void ShowStatus(Player player)
+    public void ShowStatus()
     {
+        Console.Clear();
         Console.WriteLine($"LV. {player.Level}");
         Console.WriteLine($"{player.Name} ({player.Job})");
         Console.WriteLine($"공격력 : {player.AP}");
         Console.WriteLine($"방어력 : {player.DP}");
         Console.WriteLine($"체  력 : {player.HP}");
+        Console.WriteLine($"스피드 : {player.Speed}");
         Console.WriteLine($"GOLD : {player.Gold} G");
+
+        Console.WriteLine();
+        Console.WriteLine($"     O     ← 장신구 : {player.ShowEquip(0)}");
+        Console.WriteLine($"   - | -   ← 무기구 : {player.ShowEquip(1)}");
+        Console.WriteLine($"     |     ← 방어구 : {player.ShowEquip(2)}");
+        Console.WriteLine($"    | |    ← 아이템 : {player.ShowEquip(3)}");
+
 
         Console.WriteLine("\n0. 나가기");
 
         int input = GetInput(0, 0);
-        Console.WriteLine(input);
     }
 
-    public void ShowInventory(Player player)
+    // 인벤토리 - 장비 장착 / 해제 
+    public void ShowInventory()
     {
+        Console.Clear();
         Console.WriteLine("[아이템 목록]\n");
 
-        foreach (Equip equip in player.Inventory)
+        for (int i = 0; i < 9; i++)
         {
-            Console.WriteLine($"- [{equip.State}] {equip.Name}\t\t| {equip.Effect} | \t\t{equip.Sub}");
+            if (i < player.Inventory.Count)
+            {
+                Equip equip = player.Inventory[i];
+                Console.WriteLine(string.Format("- ({0}) [{1}] {2} \t\t | {3} \t | {4} \t\t | {5}", i + 1, equip.State, equip.Name, equip.Type, equip.Effect, equip.Sub));
+            }
+            else
+            {
+                Console.WriteLine(string.Format("- ({0})", i + 1));
+            }
         }
 
-        Console.WriteLine("\n1. 장착 관리");
-        Console.WriteLine("\n0. 나가기");
+        Console.WriteLine("\n1 ~ 9. 해당 장비 장착/해제");
+        Console.WriteLine("0. 나가기");
+
+        int input = GetInput(0, player.Inventory.Count);
+        
+        if (0 < input && input <= player.Inventory.Count)
+        {
+            player.Inventory[input - 1].Equipped(player);
+            ShowInventory();
+        }
+    }
+
+    public void SetInventory()
+    {
+
     }
 }
 
@@ -128,11 +224,10 @@ internal class Program
 {
     static void Main()
     {
-        GameController GC = new GameController();
-        // GC.VillageEnterance();
-
         Player player = new Player("name", "전사", 10, 10, 100, 10);
-        GC.ShowStatus(player);
-        // GC.ShowInventory(player);
+
+        GameController GC = new GameController(player);
+
+        GC.VillageEnterance();
     }
 }
