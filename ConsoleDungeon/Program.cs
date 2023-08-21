17fc77442da8079af2f7837ac76dbbeca3172bf5
minsang <minsang0610@naver.com>
2023-08-21 14:38:28 +0900

    [ADD] String Color
    선택사항 : Yellow
    전투 : Red
    그 외 : White

--- ConsoleDungeon/Program.cs
+++ ConsoleDungeon/Program.cs
@@ -153,19 +153,6 @@ public class Shop
     }
 }
 
-public static class NPC
-{
-    public static void Talk(string _str)
-    {
-        for (int i = 0; i < _str.Length; i++)
-        {
-            Console.Write(_str[i]);
-            Thread.Sleep(25);
-        }
-        Console.WriteLine();
-    }
-}
-
 public class GameController
 {
     public Player Player { get; private set; }
@@ -197,20 +184,20 @@ public class GameController
         {
             Console.Clear();
             Console.WriteLine("====================================");
-            NPC.Talk($"스파르타 마을에 어서오세요. {Player.Name} 님");
-            NPC.Talk("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.\n");
+            Program.Talk($"스파르타 마을에 어서오세요. {Player.Name} 님");
+            Program.Talk("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.\n");
 
-            Console.WriteLine("1. 상태 보기");
-            Console.WriteLine("2. 인벤토리");
-            Console.WriteLine("3. 상점");
-            Console.WriteLine("4. 던전 입장");
-            Console.WriteLine("\n0. 게임 종료");
+            Program.ColorWriteLine("1. 상태 보기");
+            Program.ColorWriteLine("2. 인벤토리");
+            Program.ColorWriteLine("3. 상점");
+            Program.ColorWriteLine("4. 던전 입장");
+            Program.ColorWriteLine("\n0. 게임 종료");
 
-
-            int input = GetInput(0, 2);
+            int input = GetInput(0, 3);
 
             if (input == 1) ShowStatus();
             else if (input == 2) ShowInventory();
+            else if (input == 3) ShowShop();
             else return;
         }
     }
@@ -219,7 +206,7 @@ public class GameController
     public void ShowStatus()
     {
         Console.Clear();
-        Console.WriteLine($"◇--------------◇-----");
+        Console.WriteLine($"◇----------◇----------◇----------");
         Console.WriteLine($"| LV. {Player.Level:D2}");
         Console.WriteLine($"| {Player.Name}");
         Console.WriteLine($"| 직  업 : {Player.Job}");
@@ -228,14 +215,14 @@ public class GameController
         Console.WriteLine($"| 체  력 : {Player.HP}");
         Console.WriteLine($"| 스피드 : {Player.Speed}");
         Console.WriteLine($"|  GOLD  : {Player.Gold} G");
-        Console.WriteLine($"◇--------------◇-----");
+        Console.WriteLine($"◇----------◇----------◇----------");
         Console.WriteLine($"|     O     ← 장신구 : {Player.OnEquipName(0)}");
         Console.WriteLine($"|   - | -   ← 무기구 : {Player.OnEquipName(1)}");
         Console.WriteLine($"|     |     ← 방어구 : {Player.OnEquipName(2)}");
         Console.WriteLine($"|    | |    ← 아이템 : {Player.OnEquipName(3)}");
-        Console.WriteLine($"◇--------------◇-----");
+        Console.WriteLine($"◇----------◇----------◇----------");
 
-        Console.WriteLine("\n0. 나가기");
+        Program.ColorWriteLine("\n0. 나가기");
 
         _ = GetInput(0, 0);
     }
@@ -259,8 +246,8 @@ public class GameController
             }
         }
 
-        Console.WriteLine("\n1 ~ 9. 해당 장비 장착/해제");
-        Console.WriteLine("0. 나가기");
+        Program.ColorWriteLine("\n1 ~ 9. 해당 장비 장착/해제");
+        Program.ColorWriteLine("0. 나가기");
 
         int input = GetInput(0, Player.Inventory.Count);
 
@@ -270,40 +257,64 @@ public class GameController
             ShowInventory();
         }
     }
+
+    // 상점
+    public void ShowShop()
+    {
+        Console.Clear();
+        Program.Talk("필요한 아이템을 얻을 수 있는 상점입니다.\n");
+        Console.WriteLine("[보유 골드]");
+        Console.WriteLine($"{Player.Gold} G\n");
+
+        Console.WriteLine("[아이템 목록]");
+
+    }
 }
 
 internal class Program
 {
+    // 문자열을 한 글자씩 출력하는 함수
+    public static void Talk(string _str)
+    {
+        for (int i = 0; i < _str.Length; i++)
+        {
+            Console.Write(_str[i]);
+            Thread.Sleep(25);
+        }
+        Console.WriteLine();
+    }
+
+    // 문자열의 배경색과 글자색을 변경시켜 출력하는 함수
+    public static void ColorWriteLine(string _str, ConsoleColor _writeColor = ConsoleColor.Yellow, ConsoleColor _backColor = ConsoleColor.Black)
+    {
+        Console.BackgroundColor = _backColor;
+        Console.ForegroundColor = _writeColor;
+        Console.WriteLine(_str);
+        Console.ResetColor();
+    }
+
     static void Main()
     {
         GameController GC = new GameController();
 
-        NPC.Talk("당신의 이름을 알려주세요.");
+        Talk("당신의 이름을 알려주세요.\n");
         Console.Write(">> ");
         string warriorName = Console.ReadLine();
 
-        NPC.Talk("\n당신의 직업을 골라주세요.");
-        Console.WriteLine("1. 검사");
-        Console.WriteLine("2. 전사");
-        Console.WriteLine("3. 도적");
-        Console.Write(">> ");
+        Talk("\n당신의 직업을 골라주세요.\n");
+        ColorWriteLine("1. 검사");
+        ColorWriteLine("2. 전사");
+        ColorWriteLine("3. 도적");
+
         int jobChoice = GC.GetInput(1, 3);
 
-        if (jobChoice == 1)
-        {
-            Player player = new Player(warriorName, "검사", 10, 5, 100, 20);
-            GC.SetPlayer(player);
-        }
-        else if (jobChoice == 2)
-        {
-            Player player = new Player(warriorName, "전사", 10, 7, 120, 10);
-            GC.SetPlayer(player);
-        }
-        else
-        {
-            Player player = new Player(warriorName, "도적", 15, 3, 80, 30);
-            GC.SetPlayer(player);
-        }
+        Player player;
+
+        if      (jobChoice == 1) { player = new Player(warriorName, "검사", 10, 5, 100, 20); }
+        else if (jobChoice == 2) { player = new Player(warriorName, "전사", 10, 7, 120, 10); }
+        else                     { player = new Player(warriorName, "도적", 15, 3, 80, 30); }
+
+        GC.SetPlayer(player);
 
         GC.VillageEnterance();
     }
